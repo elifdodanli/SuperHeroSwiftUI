f8da1a5dc9cf95ac8272f27892976d4d42325c26
Elif <elfdodanli@gmail.com>
2024-08-18 12:23:59 +0300

    Initial Commit

---
+++ SuperHeroSwiftUI/SuperHeroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroSwiftUIApp.swift
+//  SuperHeroSwiftUI
+//
+//  Created by 🎀 Elif 🎀 on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
